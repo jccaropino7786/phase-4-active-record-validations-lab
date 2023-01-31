8f27dc4ec23f06690e629164fcc51eb7104b00fd
john Caropino <jccaropino7786@live.com>
2023-01-31 15:47:32 -0800

    commit tree 2bc2905d1a06fc1455aff8b71bf112eff36a7025

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum:250 }
+    validates :summary, length: { maximum:250 }
+    validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'] }  
+    validate :clickbait
+
+    def clickbait
+        if { in:["Won't Believe", "Secret", `Top #{number}`, "Guess"]} 
+            errors.add(:title, "must be clickbait")
+        end
+    end
 end
